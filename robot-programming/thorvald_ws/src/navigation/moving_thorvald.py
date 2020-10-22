c7f477d251b6b1e4288ff0bc8632945fc317e920
Will <wfmrohde@gmail.com>
2020-10-22 15:27:51 +0100

    removed random_walk

--- robot-programming/thorvald_ws/src/navigation/moving_thorvald.py
+++ robot-programming/thorvald_ws/src/navigation/moving_thorvald.py
@@ -1,46 +1,49 @@
 #!/usr/bin/env python
 
 import rospy
+import numpy as np
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
 class move_and_avoid:
-    def __init__(self,node_name,min_distance=3):
+    def __init__(self,node_name,min_distance=3,avoidance_angle=0.7854):
+        """
+        min_distance is the minimum distance between the robot and an obstacle before it turns to avoid.
+        avoidance_angle is the angle of the sector where collisions are checked.
+        """
         rospy.init_node(node_name)
         self.pub = rospy.Publisher("/thorvald_001/twist_mux/cmd_vel",Twist,queue_size=0)
+        self.sub = rospy.Subscriber("/thorvald_001/scan", LaserScan, self.laserscan_subscriber_callback)
         self.min_distance = min_distance
+        self.avoidance_angle = avoidance_angle
 
     def laserscan_subscriber_callback(self,data):
         """
-        These are the objects available in the laserscan message data 
-        Source: http://docs.ros.org/en/melodic/api/sensor_msgs/html/msg/LaserScan.html
-        float32 angle_min        # start angle of the scan [rad]
-        float32 angle_max           # end angle of the scan [rad]
-        float32 angle_increment  # angular distance between measurements [rad]
-        float32 time_increment   # time between measurements [seconds] - if your scanner
-                                # is moving, this will be used in interpolating position
-                                # of 3d points
-        float32 scan_time        # time between scans [seconds]
-        float32 range_min        # minimum range value [m]
-        float32 range_max        # maximum range value [m]
-        float32[] ranges         # range data [m] (Note: values < range_min or > range_max should be discarded)
-        float32[] intensities    # intensity data [device-specific units].  If your
-                                # device does not provide intensities, please leave
-                                # the array empty.
-        """
-        
-        centre_range = data.ranges[int(len(data.ranges)/2)]
-        
-        # if the robot will collide turn, otherwise go straight
-        if(centre_range < self.min_distance):
-            self.publish_cmd_vel(ang=[0,0,1.5708])
-        else:
-            self.publish_cmd_vel()
-    
-    def laserscan_subscriber(self):
-        self.sub = rospy.Subscriber("/thorvald_001/scan", LaserScan, self.laserscan_subscriber_callback)
+        processes the laserscan data and checks for collisions
 
-    def publish_cmd_vel(self,lin=[1,0,0],ang=[0,0,0]):
+        laserscan message type http://docs.ros.org/en/melodic/api/sensor_msgs/html/msg/LaserScan.html
+        For the Thorvald Robot the laserscanner is positioned so that it sweeps from -pi/2 to pi/2
+        """
+
+        num_ranges_to_sample = int(self.avoidance_angle / data.angle_increment)
+    
+        sampled_ranges = data.ranges[int((len(data.ranges) -  num_ranges_to_sample)/2):int((len(data.ranges) +  num_ranges_to_sample)/2)]
+        self.check_collisions(sampled_ranges,data.range_min) 
+    
+    def check_collisions(self,sampled_ranges,range_min):
+        # the array of sampled ranges is split into left and right sides
+        collisions = np.array(sampled_ranges) < self.min_distance 
+        right_collisions = collisions[:int(len(collisions)/2)]
+        left_collisions = collisions[int(len(collisions)/2):]
+
+        if(np.sum(left_collisions) > np.sum(right_collisions)): # more collisions to the left so turn right
+            self.publish_cmd_vel(ang=[0,0,-1.5708])  
+        elif(np.sum(left_collisions) < np.sum(right_collisions)): #more collisions to the right so turn left
+            self.publish_cmd_vel(ang=[0,0,1.5708])
+        else: #keep going forwards
+            self.publish_cmd_vel(lin=[1,0,0])
+
+    def publish_cmd_vel(self,lin=[0,0,0],ang=[0,0,0]):
         message = Twist()
         message.linear.x = lin[0]
         message.linear.y = lin[1]
@@ -53,5 +56,4 @@ class move_and_avoid:
 move_and_avoid = move_and_avoid("moving_thorvald")
 
 while not rospy.is_shutdown():
-    move_and_avoid.laserscan_subscriber()
-    rospy.sleep(0.05)
+    rospy.sleep(0.01)
